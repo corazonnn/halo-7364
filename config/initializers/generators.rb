9ad690b6ce71b05e73b2f98972be770ebfc15672
yuya.aikawa <zero7364@icloud.com>
2020-11-14 10:07:28 +0000

    second commit

---
+++ config/initializers/generators.rb
@@ -0,0 +1,6 @@
+Rails.application.config.generators do |g|
+  #g.stylesheets false　ページのデザインはブートストラップだけじゃなくてCSSでもやるからstylesheetは残しておく
+  g.javascripts false
+  g.helper false
+  g.skip_routes true
+end
