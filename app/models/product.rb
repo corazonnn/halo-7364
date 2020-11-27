ff0b7f1b9cbb062a2b286e71d8dfc16db2033c20
yuya.aikawa <zero7364@icloud.com>
2020-11-27 07:46:31 +0000

    細かなCSSの調整

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,8 @@ class Product < ApplicationRecord
   
   validates :title, presence: true, length: { maximum: 30 }
   validates :language, presence: true, length: { maximum: 255 }
-  validates :detail, length: { maximum: 255 }
-  validates :period, length: { maximum: 255 }
+  validates :detail, presence: true, length: { maximum: 255 }
+  validates :period, presence: true, length: { maximum: 255 }
  
   #あとはimageについて書くだけ。それ以外はできた
   
