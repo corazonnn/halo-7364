a701b6aa268a2cf27b9470b8215071678eabdb26
yuya.aikawa <zero7364@icloud.com>
2020-12-12 11:25:03 +0000

    レイアウトを整える

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,5 @@
 class RoomsController < ApplicationController
+  before_action :require_user_logged_in, only: [:show]
   def show
     @messages = Message.order(id: :desc).page(params[:page]).per(6)
     @message = current_user.messages.build 
