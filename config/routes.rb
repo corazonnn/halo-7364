0a4f5a4b1cee4089f6a58317d3cd7ca1282ed956
yuya.aikawa <zero7364@icloud.com>
2021-01-16 09:57:58 +0000

    管理ユーザー登録機能#8

--- config/routes.rb
+++ config/routes.rb
@@ -4,21 +4,21 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-
   get 'signup', to: 'users#new'
-
   get 'chat-room', to: 'rooms#show'
-
   get 'search', to: 'products#index'
-
   resources :users, only: %i[index show create edit update]
 
   resources :products
-
-  # ユーザが「頑張ったね！」できるようにルーティングしていく
+  #管理者用のルーティング
+  namespace :admin do
+    resources :products, only: [:index, :new, :create, :show,  :edit, :destroy]
+  end
+  
+  # ユーザ(user)がプロダクト(product)に対していいね(good)をする
   resources :goods, only: %i[create destroy]
-
+  #ユーザ(user)がプロダクト(product)に対してコメント(comment)をする
   resources :comments, only: %i[create destroy]
-
+  #ユーザ(user)がチャットグループに対して発言(message)をする
   resources :messages, only: %i[create destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,61 +10,63 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_201_210_094_834) do
-  create_table 'comments', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.string 'reply'
-    t.bigint 'user_id'
-    t.bigint 'product_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['product_id'], name: 'index_comments_on_product_id'
-    t.index ['user_id'], name: 'index_comments_on_user_id'
+ActiveRecord::Schema.define(version: 2021_01_16_062511) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "reply"
+    t.bigint "user_id"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_comments_on_product_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table 'goods', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.bigint 'user_id'
-    t.bigint 'product_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['product_id'], name: 'index_goods_on_product_id'
-    t.index ['user_id'], name: 'index_goods_on_user_id'
+  create_table "goods", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_goods_on_product_id"
+    t.index ["user_id"], name: "index_goods_on_user_id"
   end
 
-  create_table 'messages', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.text 'chat'
-    t.bigint 'user_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_messages_on_user_id'
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "chat"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
-  create_table 'products', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.bigint 'user_id'
-    t.string 'title'
-    t.string 'language'
-    t.string 'detail'
-    t.string 'period'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'option'
-    t.index ['user_id'], name: 'index_products_on_user_id'
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "title"
+    t.string "language"
+    t.string "detail"
+    t.string "period"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "option"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
-  create_table 'users', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.string 'email'
-    t.string 'password_digest'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'username'
-    t.string 'language'
-    t.string 'introduction'
-    t.string 'image'
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "language"
+    t.string "introduction"
+    t.string "image"
+    t.boolean "admin", default: false
   end
 
-  add_foreign_key 'comments', 'products'
-  add_foreign_key 'comments', 'users'
-  add_foreign_key 'goods', 'products'
-  add_foreign_key 'goods', 'users'
-  add_foreign_key 'messages', 'users'
-  add_foreign_key 'products', 'users'
+  add_foreign_key "comments", "products"
+  add_foreign_key "comments", "users"
+  add_foreign_key "goods", "products"
+  add_foreign_key "goods", "users"
+  add_foreign_key "messages", "users"
+  add_foreign_key "products", "users"
 end
